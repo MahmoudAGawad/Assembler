f9fb265843dbd0c42f514b9c65af9af59a5f4ab7
MahmoudAGawad <mahmoudsaleh272@gmail.com>
2014-05-14 15:24:35 +0200

    HelloWorld
    Show time!

---
+++ src/Assembler.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Assembler.cpp
+// Author      : Mahmoud A.Gawad, Mostafa Hamdy, Waleed Adel, Diaa Ibrahim, and Abdallah El-Abasery
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
