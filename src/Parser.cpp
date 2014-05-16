3177a06868acf46224a798441f8d94f51e6adb6a
waleedAdel <waleed.adel.mahmoud@gmail.com>
2014-05-16 07:20:26 -0800

    Parser_commit1
    waleed

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,10 +1,197 @@
-/*
- * Parser.cpp
- *
- *  Created on: May 14, 2014
- *      Author: MahmoudSaleh
- */
+#include "Parser.h"
+
+
+Parser::Parser()
+{
+    //ctor
+}
+
+Parser::~Parser()
+{
+    //dtor
+}
+
+void Parser::setParsingLine(string str){
+splittedLine=str;
+}
+
+vector<string>Parser::split(string str){
+
+    string buf;
+    stringstream ss(str);
+
+    vector<string> tokens;
+
+    while (ss >> buf)
+        tokens.push_back(buf);
+
+// for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it)
+//    cout <<*it<<'#';
+//  cout << '\n';
+
+ return tokens;
+}
+
+void Parser::connectWords(vector<string> splittedWords){
+
+ // not comment nor rsub
+
+  if(isComment(splittedWords)){
+
+   isCommentVar=true;
+  }
+
+ else if(isRsub(splittedWords)){
+    operation=splittedWords.at(0);
+ }
+
+else{
+        if(splittedWords.size()>=2){
+  connectOperand(splittedWords);
+
+     cout<<"label    "<<label<<endl;
+     cout<<"operation   " <<operation<<endl;
+     cout<<"operand    "<<operand<<endl;
+             }
+
+}
+
+
+  // then add first label to first column of array if found
+  // and operation to the second comlumn
+  // and operand to the third column
+
+}
+
+
+void Parser::connectOperand(vector<string> splittedWords){
+
+  if(checkForComma(splittedWords)){
+
+      int commaType=commaTypeMethod(splittedWords);
+  //     cout<<commaType<<endl;
+     switch(commaType){
+
+ case 1:
+        operand=splittedWords.at(splittedWords.size()-1);
+      operation=splittedWords.at(splittedWords.size()-2);
+      if(splittedWords.size()>=3){
+        label=splittedWords.at(splittedWords.size()-3);
+      }
+     break;
+ case 2:
+      operand="";
+     operand+=splittedWords.at(splittedWords.size()-3);
+     operand+=splittedWords.at(splittedWords.size()-2);
+     operand+=splittedWords.at(splittedWords.size()-1);
+
+     operation=splittedWords.at(splittedWords.size()-4);
+
+     if(splittedWords.size()>4){
+        label=splittedWords.at(splittedWords.size()-5);
+     }
+     break;
+ case 3:
+ case 4:
+        operand="";
+     operand+=splittedWords.at(splittedWords.size()-2);
+     operand+=splittedWords.at(splittedWords.size()-1);
+       operation=splittedWords.at(splittedWords.size()-3);
+     if(splittedWords.size()>3){
+        label=splittedWords.at(splittedWords.size()-4);
+     }
+     break;
+  default:
+    break;
+     }
+
+
+    cout<<"there is a comma "<<endl;
+
+      }
+else{
+      operand=splittedWords.at(splittedWords.size()-1);
+      operation=splittedWords.at(splittedWords.size()-2);
+      if(splittedWords.size()>=3){
+        label=splittedWords.at(splittedWords.size()-3);
+      }
+
+}
+
+}
+
+
+bool Parser::isComment(vector<string> splittedWords){
+
+string str=splittedWords.at(0);
+
+ if(str.at(0)=='.'){
+         return true;
+  }
+ else{
+    return false;
+ }
+
+}
+
+bool Parser::isRsub(vector<string> splittedWords){
+
+    if(splittedWords.size()==1){
+        if(splittedWords.at(0)=="RSUB"||splittedWords.at(0)=="rsub"){
+            return true;
+        }
+    }
+
+return false;
+}
+
+
+bool Parser::checkForComma(vector<string> splittedWords){
+// search for commas' location
+  if(splittedWords.size()>1){
+  string lastWord=splittedWords.at(splittedWords.size()-1);
+   if(lastWord.size()>1){
+   if(lastWord.at(lastWord.size()-2)==','){
+    return true;
+   }
+}
+
+  string beforeLastWord= splittedWords.at(splittedWords.size()-2);
+    if(beforeLastWord.size()>0){
+    if(beforeLastWord.at(beforeLastWord.size()-1)==','){
+    return true;
+   }
+  }
+  }
+return false;
+}
 
 
 
 
+int Parser::commaTypeMethod(vector<string> splittedWords){
+
+if(splittedWords.size()>1){
+  string lastWord=splittedWords.at(splittedWords.size()-1);
+   if(lastWord.size()>1){
+   if((lastWord.at(lastWord.size()-2)==',')&&(lastWord.at(0)==',')){
+                   return 4;
+   }
+   else{
+           return 1;
+   }
+}
+
+  string beforeLastWord= splittedWords.at(splittedWords.size()-2);
+    if(beforeLastWord.size()>0){
+    if((beforeLastWord.at(beforeLastWord.size()-1)==',')&&(beforeLastWord.at(0)==',')){
+            return 2;
+   }
+   else{
+     return 3;
+   }
+}
+     }
+
+return 0;
+}
--- src/Parser.h
+++ src/Parser.h
@@ -1,14 +1,36 @@
-/*
- * Parser.h
- *
- *  Created on: May 14, 2014
- *      Author: MahmoudSaleh
- */
+#ifndef PARSER_H
+#define PARSER_H
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-#ifndef PARSER_H_
-#define PARSER_H_
+class Parser
+{
+    public:
+        Parser();
+     void setParsingLine(string str);
+bool isComment(vector<string> splittedWords);
+       virtual ~Parser();
+    protected:
+    private:
+    string splittedLine;  //arr
+     string label;
+     string operation;
+     string operand;
+     bool isCommentVar;
+     bool isValidVar;
+ vector<string> split(string str);
+void connectWords(vector<string> splittedWords);
+void connectOperand(vector<string> splittedWords);
+bool isRsub(vector<string> splittedWords);
+bool checkForComma(vector<string> splittedWords);
+int commaTypeMethod(vector<string> splittedWords);
 
 
+};
 
-
-#endif /* PARSER_H_ */
+#endif // PARSER_H
