5e32d3166c019ff6957560a0657cdc6c1651d807
MostafaHamdy <mostafahamdy6@gmail.com>
2014-05-17 00:27:56 +0200

    pass1_validator

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -9,51 +9,57 @@
 #include <iostream>
 #include <sstream>
 #include "HashTable.cpp"
+#include "operationInfo.cpp"
 using namespace std;
 
 int main(){
 
-	HashTable<string,string> ht;
-	string key="Key#",num,aa,bb,val="Value#";
-	for(int i=0;i<16;i++){
-		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
-		aa = key + num;
-		bb = val + num;
-		ht.insert(aa,bb);
-	}
-	cout << ht.size()<<endl;
+//	HashTable<string,string> ht;
+//	string key="Key#",num,aa,bb,val="Value#";
+//	for(int i=0;i<16;i++){
+//		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
+//		aa = key + num;
+//		bb = val + num;
+//		ht.insert(aa,bb);
+//	}
+//	cout << ht.size()<<endl;
+//
+//	ht.printAll();
+//	for(int i=0;i<16;i++){
+//		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
+//		aa = key + num;
+//		cout << ht._delete(aa)<<endl;
+//	}
+//
+//	val = "second#";
+//	for(int i=20;i<30;i++){
+//		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
+//		aa = key + num;
+//		bb = val + num;
+//		ht.insert(aa,bb);
+//	}
+//
+//	ht.printAll();
+//	cout << ht.size()<<endl;
+//
+//	cout << (ht.containsKey("Key#22")?"Found\n":"NOT Found\n");
+//	cout << (ht.containsValue("second#22")?"Found\n":"NOT Found\n");
+//
+//	ht.iterator();
+//	Entry<string,string>* entry;
+//	while(ht.hasNext()){
+//		entry = ht.next();
+//		cout<<"Key = "<< entry->getKey()<<" | Value = "<< entry->getValue()<<endl;
+//	}
+//
+//	HashTable<int,string> a;
+//	a.insert(100,"File");
+//	a.printAll();
 
-	ht.printAll();
-	for(int i=0;i<16;i++){
-		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
-		aa = key + num;
-		cout << ht._delete(aa)<<endl;
-	}
-
-	val = "second#";
-	for(int i=20;i<30;i++){
-		num = static_cast<ostringstream*>( &(ostringstream() << i) )->str();
-		aa = key + num;
-		bb = val + num;
-		ht.insert(aa,bb);
-	}
-
-	ht.printAll();
-	cout << ht.size()<<endl;
-
-	cout << (ht.containsKey("Key#22")?"Found\n":"NOT Found\n");
-	cout << (ht.containsValue("second#22")?"Found\n":"NOT Found\n");
-
-	ht.iterator();
-	Entry<string,string>* entry;
-	while(ht.hasNext()){
-		entry = ht.next();
-		cout<<"Key = "<< entry->getKey()<<" | Value = "<< entry->getValue()<<endl;
-	}
-
-	HashTable<int,string> a;
-	a.insert(100,"File");
-	a.printAll();
+    HashTable<string , operationInfo> table;
+    operationInfo*  opInfo = new operationInfo();
+  //  cout << opInfo->directive <<" "<<opInfo->howManyOperand <<" "<<opInfo->operationName <<" "<<opInfo->operationName <<endl;
+//   operationInfo*  opInfo = new operationInfo();
 
 	return 0;
 }
--- src/Validator.h
+++ src/Validator.h
@@ -1,18 +1,24 @@
-/*
+   /*
  * Validator.h
  *
  *  Created on: May 14, 2014
  *      Author: msotafa22
  */
 
-#ifndef VALIDATOR_H_
-#define VALIDATOR_H_
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+
 #include "HashTable.cpp"
-#include "operationInfo.h"
+#include <iostream>
+#include <strings.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
 class Validator{
 
 public :
-    Validator(HashTable<string , string> opTable);
+    Validator( HashTable<int , int> opTable );
 
     Validator();  //  empty costructor
     ~Validator(); // destructor
@@ -31,6 +37,7 @@ protected :
     HashTable<string , int> sympolTable;    // the table containing all the sympols
     HashTable<string , int> operationTable; // the table containing all the informathion about the operation
     string error ;                          // the error message
+private:
 
 
 };
