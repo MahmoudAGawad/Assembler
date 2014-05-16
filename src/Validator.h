c493d94cbda5192edc70a155d6624ce2c5aef4e8
MostafaHamdy <mostafahamdy6@gmail.com>
2014-05-16 15:58:51 +0200

    the validator class
    with initial methods and variables

--- src/Validator.h
+++ src/Validator.h
@@ -2,13 +2,31 @@
  * Validator.h
  *
  *  Created on: May 14, 2014
- *      Author: MahmoudSaleh
+ *      Author: msotafa22
  */
 
 #ifndef VALIDATOR_H_
 #define VALIDATOR_H_
 
+class Validator{
 
+public :
+    Validator(HashTable<string , operationInfo> opTable);
+    Validator();  //  empty costructor
+    ~Validator(); // destructor
+
+    void checkSyntax(string label , string operation , string operand); // check the syntax of the operation
+    bool checkValid();
+    string getError();
+
+
+protected :
+    HashTable<string , int> sympolTable; // the table containing all the sympols
+    HashTable<string , operationInfo> operationTable; // the table containing all the informathion about the operation
+    string error ;                       // the error message
+
+
+};
 
 
 #endif /* VALIDATOR_H_ */
---
+++ src/operationinfo.h
@@ -0,0 +1,14 @@
+#ifndef OPERATIONINFO_H
+#define OPERATIONINFO_H
+
+
+class operationInfo
+{
+    public:
+        operationInfo();
+        ~operationInfo();
+    protected:
+    private:
+};
+
+#endif // OPERATIONINFO_H
