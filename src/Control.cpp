d05048cdd3309d4631b65e9c92c6430fdeff32bb
diaa baraka <diaa.baraka788@gmail.com>
2014-05-17 21:32:14 +0200

    Still Working on

--- src/Control.cpp
+++ src/Control.cpp
@@ -1,45 +1,102 @@
 
 #include <iostream>
 #include <sstream>
-
-#include "Parser.cpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include "Parser.h"
 #include "Validator.h"
+#include "Reading.cpp"
+#include <iomanip>
 
 #include <fstream>
 #include <string>
 using namespace std;
-
+string completeString(string str,int len)
+{
+    int l=str.length();
+    for(int i=l; i<len; i++)
+    {
+        str+=" ";
+    }
+    return str;
+}
+string completeAddress(string str)
+{
+    int l=str.length();
+    for(int i=l; i<6; i++)
+    {
+        str="0"+str;
+    }
+    return str;
+}
 int main()
 {
     Parser parser;//Paser OBJECT
+    Reading read;
+    HashTable<string,operationInfo>*opTable =  read.read();
 
-//    Validator validator;
-
+    Validator *validator=new Validator(*opTable);
+    string label;
+    string operation;
+    string operand;
+    string address;
     string line;
     ifstream myfile ("src.txt");
     ofstream outputFile;
     outputFile.open("LISAFILE.txt");
+    outputFile<<">>  Source Program statements with value of LC indicated\n"<<endl;
     if (myfile.is_open())
     {
-        while ( getline (myfile,line) )
+        while (! myfile.eof() )
         {
+            line="";
+            getline (myfile,line);
 
             parser.setParsingLine(line);
             parser.parse();
-            cout<<parser.isComment();
-            cout<<line<<endl;
+
             if(parser.isComment()||parser.isInValid())
             {
-                outputFile << line << endl;
+                outputFile << "      "<<line << endl;
+
             }
             else
+
             {
-                cout<<line<<endl;
+                label=parser.getLabel();
+                operation=parser.getOperation();
+                operand=parser.getOperand();
+//                cout<<label<<"\t"<<operation<<"\t"<<operand<<endl;
+                validator->validateSyntax(label,operation,operand);
+
+
+                label= completeString(label,9);
+                operation=completeString(operation,8);
+
+                cout<<validator->getAddress()<<endl;
+                address=completeAddress(validator->getAddress());
+                outputFile <<address<<label<<operation<<operand<<endl;
+                if(!validator->isValid())
+                {
+                    outputFile<<"\t\t"<<validator->getError()<<endl;
+                }
 
             }
 
 
         }
+        outputFile<<"\n>>    e n d    o f   p a s s   1\n"<<endl;
+        outputFile<<">>   *****************************************************"<<endl;
+        outputFile<<">>    s y m b o l     t a b l e   (values in decimal)\n"<<endl;
+        outputFile<<"        name         value"<<endl;
+        outputFile<<"        ------------------"<<endl;
+
+
+
+
+        outputFile<<"\n>>   *****************************************************"<<endl;
+
+
         myfile.close();
         outputFile.close();
 
@@ -54,3 +111,5 @@ int main()
 
     return 0;
 }
+
+
